dccecacdab49c9e2f3b0ec9f556d4448bdc6e356
warmrome <pxpx786@gmail.com>
2021-11-26 15:23:48 -0800

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Jameson Hurst on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
